b3090a9d854aac4e0f780786d3b500a9f5d24a99
nope nope <nope nope>
2016-12-06 17:56:35 +0100

    construcotr terminer

--- tmp.js
+++ tmp.js
@@ -1,7 +1,7 @@
 class VueGrille{
     constructor(){
         let grille = document.createElement('table');
-        for(let i = 0; i < 9; i++){
+        for(let i = 0; i < 3; i++){
             let ligne = document.createElement('tr');
             for(let j = 0; j < 3; j++){
                 let cases = document.createElement('td');
@@ -11,12 +11,16 @@ class VueGrille{
 
                 let caseIa = document.createElement('div');
                 caseIa.className = "forme ia";
+                cases.addEventListener('click');
+
 
                 cases.appendChild(casePlayer);
                 cases.appendChild(caseIa);
                 ligne.appendChild(cases);
             }
+            grille.appendChild(ligne);
         }
+        document.body.appendChild(grille);
     }
 }
 
